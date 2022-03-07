5d9d619c4a0265a3f1b408b8ed8bfe3511d0a7c0
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-07 08:36:05 -0600

    updated some of the connections

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+
 
 
 
