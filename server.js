1f05762ee2b2fc39f44dd3013e1e1e3930223019
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-03-08 01:14:04 -0600

    fixed missing session info causing 400 error on user create

--- server.js
+++ server.js
@@ -3,23 +3,33 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const express = require('express');
 const routes = require('./controllers');
-// import sequelize connection
 const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({  });
 
+const sess = {
+    secret: process.env.SESSION_PW,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
+
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
 });
 
