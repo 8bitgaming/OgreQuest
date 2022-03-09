4b22a0796ef27025e7bb32cd5d500d953027bd06
dwetoskey <dwetoskey224@gmail.com>
2022-03-09 16:18:39 -0500

    fixed home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,11 +4,17 @@ const { Character, User, Monsters } = require('../models');
 
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    });
 });
 
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login');
 });
 
