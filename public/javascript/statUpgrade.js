bd2b220a29d56aa2ed67af3c3f8d65857066910d
Armando <cuelarmando@yahoo.com>
2022-03-11 12:06:50 -0500

    added stat upgrade

--- public/javascript/statUpgrade.js
+++ public/javascript/statUpgrade.js
@@ -1,29 +1,63 @@
-const character_name = document.querySelector('#character-name').value.trim();
-const userID = document.querySelector('#character-name').getAttribute('user');
-const goldValue = document.querySelector("input[name='difficulty']:checked").value
+var character_name = document.querySelector(".upgrade-form").getAttribute('name');
+const character_health = document.querySelector(".upgrade-form").getAttribute('health');
+const character_gold = document.querySelector(".upgrade-form").getAttribute('gold');
+const character_attack = document.querySelector(".upgrade-form").getAttribute('attack');
+const userID = document.querySelector(".upgrade-form").getAttribute('user');
 
-const addHealth = () => {
+
+const addHealth = (event) => {
+    let newGold = parseInt(character_gold) - 10;
+    if (newGold < 0) {
+        window.alert("you dont have enough gold");
+        return;
+    }
+    else {
+        let newHealth = parseInt(character_health) + 10;
+        let newAttack = parseInt(character_attack);
+        submitUpgrade(newAttack, newGold, newHealth);
+
+    }
+    location.replace('/');
 
 }
-const removeHealth = () => {
+
+const addAttack = (event) => {
+    let newGold = parseInt(character_gold) - 10;
+    if (newGold < 0) {
+        window.alert("you dont have enough gold");
+        return;
+    }
+    else {
+        let newAttack = parseInt(character_attack) + 10;
+        let newHealth = parseInt(character_health);
+        submitUpgrade(newAttack, newGold, newHealth);
+
+    }
+    location.replace('/');
 
 }
-const addAttack = () => {
 
-}
-const removeAttack = () => {
 
+const submitUpgrade = (newAttack, newGold, newHealth) => {
+    const response = fetch('/api/characters/' + parseInt(userID), {
+        method: 'PUT',
+        body: JSON.stringify({
+            attack: newAttack,
+            gold: newGold,
+            hp: newHealth,
+
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.reload();
+
+    }
 }
-// const submitUpgrade = () => {
-//     const response = await fetch('/api/characters/'+ userID, {
-//         method: 'PUT',
-//         body: JSON.stringify({
-//             name: character_name,
-//             user_id: parseInt(userID),
-//             gold: difficultValue,
-//         }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
-// }
+
+
+
+document.querySelector('.addAttack').addEventListener('click', addAttack);
+document.querySelector('.addHealth').addEventListener('click', addHealth);
