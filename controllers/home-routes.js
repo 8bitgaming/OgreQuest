f1d6c91ac1bfdd1aa8bb8dd5d75af69b2ca2a4bb
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-03-11 18:12:44 -0600

    ability to delete characters

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,7 +39,7 @@ router.get('/character', (req, res) => {
         include: [
             {
                 model: Character,
-                attributes: ['name', 'gold'],
+                attributes: ['id','name', 'gold', 'attack'],
             }
         ]
     })
---
+++ public/javascript/deleteCharacter.js
@@ -0,0 +1,21 @@
+const charId = document.querySelector(".delete-character").getAttribute('charId');
+const userId = document.querySelector(".delete-character").getAttribute('userId');
+
+
+const deleteCharacter = async (event) => {
+    if (charId && userId ) {
+        const response = await fetch(`/api/characters/${charId}`, {
+            method: 'delete',
+        
+    })
+        if (response.ok) {
+            location.replace('/');
+            document.location.reload();
+
+        }
+    }
+}
+
+// document.querySelectorAll(".delete-character").addEventListener('click', deleteCharacter);
+
+document.querySelectorAll('.delete-character').forEach(e => e.addEventListener('click', deleteCharacter))
